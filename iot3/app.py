f61c610f036553c25c95249350e0011013c4b08b
Rodrup9 <102837219+Rodrup9@users.noreply.github.com>
2024-01-29 16:27:15 -0600

    Add files via upload

--- iot3/app.py
+++ iot3/app.py
@@ -3,46 +3,69 @@ from flask_socketio import SocketIO
 import cv2
 import base64
 import threading
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Inicializa la captura de video desde la cámara (ajusta el índice según tu configuración)
-cap = cv2.VideoCapture(0)
+video_path = 'carPark.mp4'
+cap = cv2.VideoCapture(video_path)
 
-# Contador global
+with open('CarParkPos', 'rb') as f:
+    posList = pickle.load(f)
+width, height = 107, 48
+
 frame_count = 0
 
-def video_stream():
+def check_parking_space(frame):
     global frame_count
+
+    space_counter = 0
+
+    for pos in posList:
+        x, y = pos
+        img_crop = frame[y: y + height, x: x + width]
+        count = cv2.countNonZero(cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY))
+
+        if count < 950:
+            color = (0, 255, 0)
+            thickness = 5
+            space_counter += 1
+        else:
+            color = (0, 0, 255)
+            thickness = 2
+
+        cv2.rectangle(frame, pos, (pos[0] + width, pos[1] + height), color, thickness)
+        cv2.putText(frame, str(count), (x, y + height - 3), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+
+    cv2.putText(frame, f'Free spaces: {space_counter}/{len(posList)}', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 210, 0), 5, cv2.LINE_AA)
+
+    frame_count += 1
+
+    _, buffer = cv2.imencode('.jpg', frame)
+    jpg_data = base64.b64encode(buffer).decode('utf-8')
+
+    socketio.emit('video_frame', {'frame': jpg_data, 'count': frame_count})
+    socketio.sleep(0.1)
+
+def video_stream():
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Incrementa el contador
-        frame_count += 1
+        img_threshold = cv2.adaptiveThreshold(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
+        img_median = cv2.medianBlur(img_threshold, 5)
+        kernel = np.ones((3, 3), np.uint8)
+        img_dilate = cv2.dilate(img_median, kernel, 1)
 
-        # Dibuja el contador en la esquina superior izquierda del video
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame, f'Frame Count: {frame_count}', (10, 30), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        check_parking_space(frame)
 
-        # Codifica el marco a formato JPEG
-        _, buffer = cv2.imencode('.jpg', frame)
-        jpg_data = base64.b64encode(buffer).decode('utf-8')
-
-        # Envía el marco codificado y el contador a través de WebSocket
-        socketio.emit('video_frame', {'frame': jpg_data, 'count': frame_count})
-
-        # Espera un breve período de tiempo para no sobrecargar la conexión
-        socketio.sleep(0.1)
-
-# Ruta principal para mostrar la página web
 @app.route('/')
 def index():
     return render_template('index2.html')
 
-# Manejador de conexión para iniciar la transmisión de video en un hilo separado
 @socketio.on('connect')
 def handle_connect():
     if not hasattr(socketio, 'thread'):
